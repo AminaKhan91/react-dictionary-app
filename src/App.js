9cc23d717d18c1efdb9d1f3fef97cbd7c075b368
AminaKhan91 <96996241+AminaKhan91@users.noreply.github.com>
2022-05-09 21:51:02 +0100

    Rendered the word meaning

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" img-fluid alt="logo" />
+        <img src={logo} className="App-logo" img-fluid="true" alt="logo" />
       </header>
       <div className="container">
         <main>
