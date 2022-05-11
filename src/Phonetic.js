9ed0c115c5b6a353c8a9897bc655cabca5f69539
AminaKhan91 <96996241+AminaKhan91@users.noreply.github.com>
2022-05-11 22:37:35 +0100

    Updated styling and loop for phonetics, also added pictures Api for these to show for whatever is searched for. Also styled this

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,11 +4,10 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <span className="text"> {props.phonetic.text} </span>
-
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
+      <span className="text"> {props.phonetic.text} </span>
       <br />
     </div>
   );
